37e5c9950205e9a6508dba4cb188e98ba39c6420
ayaz7964 <ayazhussain.bssef22@iba-suk.edu.pk>
2026-01-16 12:21:30 +0500

    first commit

---
+++ src/component/LandingPage.jsx
@@ -0,0 +1,7 @@
+export default function LandingPage() {
+    return (
+        <div className="text-center mt-20 text-3xl text-green-800 font-bold bg-red-200 p-10 rounded-lg shadow-lg">
+            Hello Ayaz Hussain How Are you ? 
+        </div>
+)
+}
